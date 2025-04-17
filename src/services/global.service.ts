c93f5dd8bf1051fb00473515b3929c5952b1f82d
MFarhanZ1 <farhanaulia.p@gmail.com>
2025-04-17 14:57:25 +0700

    ✨ refactor: update response types in GlobalService methods for consistency

--- src/services/global.service.ts
+++ src/services/global.service.ts
@@ -1,27 +1,25 @@
-import { GlobalHealthResponse, GlobalIntroduceResponse } from "../types/global.type"
+import { GlobalServiceHealthResponse, GlobalServiceIntroduceResponse } from "../types/global.type"
 
 export default class GlobalService {
 
-    public static async introduce(): Promise<GlobalIntroduceResponse> {
-        const introduceMessage: GlobalIntroduceResponse = {
+    public static async introduce(): Promise<GlobalServiceIntroduceResponse> {
+        return {
             'response': true,
             'message': 'Cihuy, Halow Semua 👋 ~ Selamat datang di API Setoran Hafalan! 🎉',
             'version': process.env.APP_VERSION || '1.0.0',
             'contributor': 'https://github.com/MFarhanZ1/hono-api-setoran-hafalan',
             'timezone': `Asia/Jakarta ~ ${new Date().toLocaleString('id-ID', { timeZone: 'Asia/Jakarta' })} WIB`
         }
-        return introduceMessage;
     }
 
-    public static async health(): Promise<GlobalHealthResponse> {
-        const healthMessage: GlobalHealthResponse = {
+    public static async health(): Promise<GlobalServiceHealthResponse> {
+        return {
             'response': true,
             'message': 'Cihuy, API Setoran Hafalan sehat-sehat saja! 😁',
             'status': 'OK',
             'uptime': process.uptime(),
             'memoryUsage': process.memoryUsage()
         }
-        return healthMessage;
     }
-    
+
 }
