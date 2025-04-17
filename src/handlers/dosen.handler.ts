39a5e011f97545dd4ed3589ec5665715faeab311
MFarhanZ1 <farhanaulia.p@gmail.com>
2025-04-17 14:55:42 +0700

    🔥 feat: implement DosenHandler with getPASaya method for user email retrieval

---
+++ src/handlers/dosen.handler.ts
@@ -0,0 +1,13 @@
+import { Context } from "hono";
+import DosenService from "../services/dosen.service";
+import { APIError } from "../utils/api_error.util";
+
+export default class DosenHandler {
+
+    public static async getPASaya(c: Context) {
+        const { email } = c.get("user");
+        if (!email) throw new APIError("Waduh, email kamu kosong cuy! 😭", 404);
+        return c.json(await DosenService.getPASaya(email));
+    }
+    
+}
