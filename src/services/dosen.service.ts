0a2f0ada4f546220374cfdaeefb6107b19eee049
MFarhanZ1 <farhanaulia.p@gmail.com>
2025-04-17 14:57:07 +0700

    🔥 feat: implement DosenService with getPASaya method for retrieving detailed Dosen and student information

---
+++ src/services/dosen.service.ts
@@ -0,0 +1,25 @@
+import DosenRepository from "../repositories/dosen.repository";
+import { DosenServiceGetPASayaResponse } from "../types/dosen.type";
+import { APIError } from "../utils/api_error.util";
+
+export default class DosenService {
+
+    public static async getPASaya(email: string): Promise<DosenServiceGetPASayaResponse> {
+        const infoDasarDosen = await DosenRepository.findByEmail(email);
+        if (!infoDasarDosen) {
+            throw new APIError('Dosen tidak ditemukan! 😭', 404);
+        }
+        const ringkasanMahasiswaPAPerAngkatan = await DosenRepository.findRingkasanMahasiswaPAPerAngkatanByEmail(email);              
+        return {
+            response: true,
+            message: 'Berikut info dosen lengkap serta detail mahasiswa per angkatan (max 8 akt)! 😁',
+            data: {
+                info_dasar: infoDasarDosen,
+                info_mahasiswa_pa: {
+                    ringkasan: ringkasanMahasiswaPAPerAngkatan
+                },
+            },
+        };
+    }
+    
+}
