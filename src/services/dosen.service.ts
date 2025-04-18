51c256530786758553e020d5458568e2078c492a
MFarhanZ1 <farhanaulia.p@gmail.com>
2025-04-18 11:53:30 +0700

    ✨ refactor: update getPASaya method to use structured request and response types

--- src/services/dosen.service.ts
+++ src/services/dosen.service.ts
@@ -1,22 +1,31 @@
 import DosenRepository from "../repositories/dosen.repository";
-import { DosenServiceGetPASayaResponse } from "../types/dosen.type";
+import MahasiswaRepository from "../repositories/mahasiswa.repository";
+import { FindByEmailReturnInterface } from "../types/dosen/repository.type";
+import { GetPASayaRequestInterface, GetPASayaResponseInterface } from "../types/dosen/service.type";
 import { APIError } from "../utils/api-error.util";
 
 export default class DosenService {
 
-    public static async getPASaya(email: string): Promise<DosenServiceGetPASayaResponse> {
-        const infoDasarDosen = await DosenRepository.findByEmail(email);
+    public static async getPASaya({email}: GetPASayaRequestInterface): Promise<GetPASayaResponseInterface> {
+        
+        // Ambil dulu info dasar dari dosen berdasarkan email
+        const infoDasarDosen: FindByEmailReturnInterface | null = await DosenRepository.findByEmail({email});
         if (!infoDasarDosen) {
             throw new APIError('Dosen tidak ditemukan! 😭', 404);
         }
-        const ringkasanMahasiswaPAPerAngkatan = await DosenRepository.findRingkasanMahasiswaPAPerAngkatanByEmail(email);              
+        const { nip } = infoDasarDosen;
+        const ringkasanMahasiswaPAPerAngkatan = await MahasiswaRepository.findRingkasanMahasiswaPAPerAngkatanByDosenNIP({nip});
+
+        const daftarMahasiswa = await MahasiswaRepository.findDaftarMahasiswaPAByDosenNIP({nip});        
+
         return {
             response: true,
             message: 'Berikut info dosen lengkap serta detail mahasiswa per angkatan (max 8 akt)! 😁',
             data: {
                 info_dasar: infoDasarDosen,
                 info_mahasiswa_pa: {
-                    ringkasan: ringkasanMahasiswaPAPerAngkatan
+                    ringkasan: ringkasanMahasiswaPAPerAngkatan,
+                    daftar_mahasiswa: daftarMahasiswa,
                 },
             },
         };
