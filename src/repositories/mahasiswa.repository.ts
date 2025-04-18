c5d91708acaee742c364958f28a4f553dbf359a8
MFarhanZ1 <farhanaulia.p@gmail.com>
2025-04-18 11:54:22 +0700

    ✨ feat: implement MahasiswaRepository with methods for email lookup and student summaries by Dosen NIP

---
+++ src/repositories/mahasiswa.repository.ts
@@ -0,0 +1,47 @@
+import prisma from "../infrastructures/db.infrastructure";
+import { FindByEmailParamsInterface, FindByEmailReturnInterface, FindDaftarMahasiswaPAByDosenNIPParamsInterface, FindDaftarMahasiswaPAByDosenNIPReturnInterface, FindRingkasanMahasiswaPAPerAngkatanByDosenNIPParamsInterface, FindRingkasanMahasiswaPAPerAngkatanByDosenNIPReturnInterface } from "../types/mahasiswa/repository.type";
+
+export default class MahasiswaRepository {
+    
+    public static async findByEmail({email}: FindByEmailParamsInterface): Promise<FindByEmailReturnInterface | null> {
+        return await prisma.mahasiswa.findUnique({
+            where: {
+                email: email
+            }
+        })
+    }
+
+    public static async findRingkasanMahasiswaPAPerAngkatanByDosenNIP({nip}: FindRingkasanMahasiswaPAPerAngkatanByDosenNIPParamsInterface): Promise<FindRingkasanMahasiswaPAPerAngkatanByDosenNIPReturnInterface[]> {
+        return await prisma.$queryRaw`
+            SELECT 
+                CONCAT('20', substring(nim FROM 2 FOR 2)) AS tahun, 
+                COUNT(substring(nim FROM 2 FOR 2))::int AS total 
+            FROM 
+                mahasiswa 
+            WHERE 
+                nip=${nip}
+            GROUP BY 
+                substring(nim FROM 2 FOR 2) 
+            ORDER BY 
+                substring(nim FROM 2 FOR 2) DESC
+            LIMIT 8;
+        `
+    }
+
+    public static async findDaftarMahasiswaPAByDosenNIP({nip}: FindDaftarMahasiswaPAByDosenNIPParamsInterface): Promise<FindDaftarMahasiswaPAByDosenNIPReturnInterface[]> {
+        return await prisma.$queryRaw`
+            SELECT mahasiswa.email,
+				mahasiswa.nim, 
+				mahasiswa.nama,
+				CONCAT('20', SUBSTRING(mahasiswa.nim FROM 2 FOR 2)) as angkatan,
+				CASE 
+					WHEN EXTRACT(MONTH FROM CURRENT_DATE) >= 8 THEN (SUBSTRING(EXTRACT(YEAR FROM CURRENT_DATE)::text FROM 2 FOR 4)::int - SUBSTRING(mahasiswa.nim FROM 2 FOR 2)::int) * 2 + 1
+					ELSE (SUBSTRING(EXTRACT(YEAR FROM CURRENT_DATE)::text FROM 2 FOR 4)::int - SUBSTRING(mahasiswa.nim FROM 2 FOR 2)::int) * 2
+				END as semester
+            FROM mahasiswa
+            WHERE mahasiswa.nip = ${nip}
+            ORDER BY SUBSTRING(mahasiswa.nim FROM 2 FOR 2) DESC, mahasiswa.nama ASC;
+        `
+    }
+
+}
