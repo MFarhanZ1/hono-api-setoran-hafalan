d9d97dedd913cea4dcb3976bc2244dbd41e5df12
MFarhanZ1 <farhanaulia.p@gmail.com>
2025-04-18 11:53:46 +0700

    ✨ refactor: update findByEmail method to use structured parameters and return types

--- src/repositories/dosen.repository.ts
+++ src/repositories/dosen.repository.ts
@@ -1,34 +1,14 @@
-import { Prisma, PrismaClient } from "../generated/prisma";
-import { DefaultArgs } from "../generated/prisma/runtime/library";
-import { DosenRepositoryFindByEmailInterface, DosenRepositoryFindRingkasanMahasiswaPAPerAngkatanByEmailInterface } from "../types/dosen.type";
-
-const prisma: PrismaClient<Prisma.PrismaClientOptions, never, DefaultArgs> = new PrismaClient();
+import prisma from "../infrastructures/db.infrastructure";
+import { FindByEmailReturnInterface, FindByEmailParamsInterface } from "../types/dosen/repository.type";
 
 export default class DosenRepository {
     
-    public static async findByEmail(email: string): Promise<DosenRepositoryFindByEmailInterface | null> {
+    public static async findByEmail({email}: FindByEmailParamsInterface): Promise<FindByEmailReturnInterface | null> {
         return await prisma.dosen.findUnique({
             where: {
                 email: email
             }
         })
-    }
-
-    public static async findRingkasanMahasiswaPAPerAngkatanByEmail(email: string): Promise<DosenRepositoryFindRingkasanMahasiswaPAPerAngkatanByEmailInterface[]> {
-        return await prisma.$queryRaw`
-            SELECT 
-				CONCAT('20', substring(nim FROM 2 FOR 2)) AS tahun, 
-				COUNT(substring(nim FROM 2 FOR 2))::int AS total 
-			FROM 
-				mahasiswa 
-			WHERE 
-				nip=(SELECT nip FROM dosen WHERE email = ${email})
-			GROUP BY 
-				substring(nim FROM 2 FOR 2) 
-			ORDER BY 
-				substring(nim FROM 2 FOR 2) DESC
-			LIMIT 8;
-        `
     }
 
 }
