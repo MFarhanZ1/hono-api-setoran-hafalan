12111a671ed930c64c736fafe06f26d21a575e77
MFarhanZ1 <farhanaulia.p@gmail.com>
2025-04-20 17:02:49 +0700

    fix: correct typo in persentase_progress_setor and add missing semicolons in interfaces

--- src/types/setoran/repository.type.ts
+++ src/types/setoran/repository.type.ts
@@ -2,14 +2,14 @@ export interface InfoSetoranInterface {
 	total_wajib_setor?: number;
 	total_sudah_setor?: number;
 	total_belum_setor?: number;
-	persentase_progress_setor?: number;
+	persentase_progres_setor?: number;
 	terakhir_setor?: string;
 	tgl_terakhir_setor?: Date | null;
 }
 
 export interface FindAllRingkasanByNIMReturnInterface {
 	nim: string;
-	info_setoran: InfoSetoranInterface
+	info_setoran: InfoSetoranInterface;
 }
 
 export interface FindAllRingkasanByNIMParamsInterface {
@@ -20,6 +20,35 @@ export interface FindRingkasanByNIMParamsInterface {
 	nim: string;
 }
 
-export interface FindRingkasanByNIMReturnInterface extends InfoSetoranInterface {
+export interface FindRingkasanByNIMReturnInterface
+	extends InfoSetoranInterface {}
 
+export interface FindRingkasanPerSyaratByNIMParamsInterface {
+	nim: string;
+}
+
+export interface FindRingkasanPerSyaratByNIMReturnInterface
+	extends InfoSetoranInterface {
+	label: string;
+}
+
+export interface FindDetailByNIMParamsInterface {
+	nim: string;
+}
+
+export interface FindDetailByNIMReturnInterface {
+	nomor: number;
+	nama: string;
+	label: string;
+	sudah_setor: boolean;
+	info_setoran: {
+		id?: string;
+		tgl_setoran?: Date;
+		tgl_validasi?: Date;
+		dosen_yang_mengesahkan: {
+			nip?: string;
+			nama?: string;
+			email?: string;
+		}
+	} | null;
 }
