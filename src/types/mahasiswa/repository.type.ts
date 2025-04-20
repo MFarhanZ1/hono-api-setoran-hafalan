d88de982159df186de850525c61740b5f9205d7a
MFarhanZ1 <farhanaulia.p@gmail.com>
2025-04-20 08:54:12 +0700

    refactor: rename FindDaftarMahasiswaPAByDosenNIP interfaces to FindAllMahasiswaPAByDosenNIP for consistency

--- src/types/mahasiswa/repository.type.ts
+++ src/types/mahasiswa/repository.type.ts
@@ -1,34 +1,34 @@
 export interface FindByEmailParamsInterface {
-    nim: string;
-    nama: string;
-    email: string;
-    nip: string;
+	nim: string;
+	nama: string;
+	email: string;
+	nip: string;
 }
 
 export interface FindByEmailReturnInterface {
-    nim: string;
-    nama: string;
-    email: string;
-    nip: string;
+	nim: string;
+	nama: string;
+	email: string;
+	nip: string;
 }
 
 export interface FindRingkasanMahasiswaPAPerAngkatanByDosenNIPParamsInterface {
-    nip: string;
+	nip: string;
 }
 
 export interface FindRingkasanMahasiswaPAPerAngkatanByDosenNIPReturnInterface {
-    tahun: string;
-    total: number;
+	tahun: string;
+	total: number;
 }
 
-export interface FindDaftarMahasiswaPAByDosenNIPParamsInterface {
-    nip: string;
+export interface FindAllMahasiswaPAByDosenNIPParamsInterface {
+	nip: string;
 }
 
-export interface FindDaftarMahasiswaPAByDosenNIPReturnInterface {
-    email: string;
-    nim: string;
-    nama: string;
-    angkatan: string;
-    semester: number;
+export interface FindAllMahasiswaPAByDosenNIPReturnInterface {
+	email: string;
+	nim: string;
+	nama: string;
+	angkatan: string;
+	semester: number;
 }
