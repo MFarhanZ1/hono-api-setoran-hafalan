81119fee1a813ee3f1f30144f1f8abb3b48a5a82
MFarhanZ1 <farhanaulia.p@gmail.com>
2025-04-20 14:02:34 +0700

    feat: add interfaces for fetching mahasiswa data by email and NIM

---
+++ src/types/mahasiswa/service.type.ts
@@ -0,0 +1,36 @@
+import { FindAllMahasiswaPAByDosenNIPReturnInterface, FindRingkasanMahasiswaPAPerAngkatanByDosenNIPReturnInterface } from "./repository.type";
+
+export interface GetByEmailRequestInterface {
+    email: string;
+}
+
+export interface GetByEmailResponseInterface {
+    nim: string;
+    nama: string;
+    email: string;    
+	angkatan: string;
+	semester: number;
+    nip_dosen_pa: string;
+}
+
+export interface GetByNIMRequestInterface {
+    nim: string;
+}
+
+export interface GetByNIMResponseInterface {
+    nim: string;
+    nama: string;
+    email: string;    
+	angkatan: string;
+	semester: number;
+    nip_dosen_pa: string;
+}
+
+export interface GetInfoMahasiswaPAByNIPRequestInterface {
+    nip: string;
+}
+
+export interface GetInfoMahasiswaPAByNIPResponseInterface {
+    ringkasan: FindRingkasanMahasiswaPAPerAngkatanByDosenNIPReturnInterface[];
+    daftar: FindAllMahasiswaPAByDosenNIPReturnInterface[];
+}
