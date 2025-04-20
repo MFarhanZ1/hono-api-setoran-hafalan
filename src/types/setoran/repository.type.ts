688a0df363b841b6e1469ca7888733f55b243a47
MFarhanZ1 <farhanaulia.p@gmail.com>
2025-04-20 08:54:24 +0700

    feat: add interfaces for InfoSetoran and FindAllRingkasanByNIM for handling setoran data

---
+++ src/types/setoran/repository.type.ts
@@ -0,0 +1,17 @@
+export interface InfoSetoranInterface {
+	total_wajib_setor?: number;
+	total_sudah_setor?: number;
+	total_belum_setor?: number;
+	persentase_progress_setor?: number;
+	terakhir_setor: string;
+	tgl_terakhir_setor?: Date | null;
+}
+
+export interface FindAllRingkasanByNIMReturnInterface {
+	nim: string;
+	info_setoran: InfoSetoranInterface
+}
+
+export interface FindAllRingkasanByNIMParamsInterface {
+	listNIM: string[];
+}
