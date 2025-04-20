ccddb140b7fddfe52741ee1a644190deb7da6116
MFarhanZ1 <farhanaulia.p@gmail.com>
2025-04-20 20:56:50 +0700

    feat: add deleteSetoranMahasiswa route for handling setoran deletion by NIM

--- src/routes/setoran.route.ts
+++ src/routes/setoran.route.ts
@@ -3,7 +3,7 @@ import { RegExpRouter } from "hono/router/reg-exp-router";
 import SetoranHandler from "../handlers/setoran.handler";
 import AuthMiddleware from "../middlewares/auth.middleware";
 import { zValidator } from "@hono/zod-validator";
-import { postSetoranSchema } from "../validators/setoran.validator";
+import { deleteSetoranSchema, postSetoranSchema } from "../validators/setoran.validator";
 import { zodError } from "../utils/zod-error.util";
 
 const setoranRoute = new Hono({ router: new RegExpRouter() });
@@ -28,6 +28,12 @@ setoranRoute.post(
 	zValidator("json", postSetoranSchema, zodError),
 	AuthMiddleware.JWTBearerTokenExtraction,
 	SetoranHandler.postSetoranMahasiswa
+);
+setoranRoute.delete(
+	"/mahasiswa/setoran/:nim",
+	zValidator("json", deleteSetoranSchema, zodError),
+	AuthMiddleware.JWTBearerTokenExtraction,
+	SetoranHandler.deleteSetoranMahasiswa
 );
 
 export default setoranRoute;
