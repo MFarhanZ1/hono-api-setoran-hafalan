77326b95a87f58e024520501066fa32d8f72c099
MFarhanZ1 <farhanaulia.p@gmail.com>
2025-04-20 14:03:16 +0700

    feat: add getByNIP method to DosenService for fetching data by NIP

--- src/services/dosen.service.ts
+++ src/services/dosen.service.ts
@@ -1,8 +1,11 @@
 import DosenRepository from "../repositories/dosen.repository";
-import { GetByEmailRequestInterface, GetByEmailResponseInterface } from "../types/dosen/service.type";
+import { GetByEmailRequestInterface, GetByEmailResponseInterface, GetByNIPRequestInterface, GetByNIPResponseInterface } from "../types/dosen/service.type";
 
 export default class DosenService {
     public static async getByEmail({email}: GetByEmailRequestInterface): Promise<GetByEmailResponseInterface | null> {
         return await DosenRepository.findByEmail({email});
+    }
+    public static async getByNIP({nip}: GetByNIPRequestInterface): Promise<GetByNIPResponseInterface | null> {
+        return await DosenRepository.findByNIP({nip});
     }
 }
