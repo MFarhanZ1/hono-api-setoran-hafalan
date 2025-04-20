5e64fab25bc167c43ebd62dc5134043bee01dab6
MFarhanZ1 <farhanaulia.p@gmail.com>
2025-04-20 17:02:26 +0700

    feat: add getAllByNIP method to DosenService for retrieving multiple Dosen records by NIP

--- src/services/dosen.service.ts
+++ src/services/dosen.service.ts
@@ -1,11 +1,16 @@
 import DosenRepository from "../repositories/dosen.repository";
-import { GetByEmailRequestInterface, GetByEmailResponseInterface, GetByNIPRequestInterface, GetByNIPResponseInterface } from "../types/dosen/service.type";
+import { GetAllByNIPRequestInterface, GetAllByNIPResponseInterface, GetByEmailRequestInterface, GetByEmailResponseInterface, GetByNIPRequestInterface, GetByNIPResponseInterface } from "../types/dosen/service.type";
 
 export default class DosenService {
     public static async getByEmail({email}: GetByEmailRequestInterface): Promise<GetByEmailResponseInterface | null> {
         return await DosenRepository.findByEmail({email});
     }
+
     public static async getByNIP({nip}: GetByNIPRequestInterface): Promise<GetByNIPResponseInterface | null> {
         return await DosenRepository.findByNIP({nip});
+    }
+
+    public static async getAllByNIP({listNIP}: GetAllByNIPRequestInterface): Promise<GetAllByNIPResponseInterface[] | null> {
+        return await DosenRepository.findAllByNIP({listNIP});
     }
 }
