0bc08ee256dcce727317c4d8299b3622ec2e045e
MFarhanZ1 <farhanaulia.p@gmail.com>
2025-04-20 17:02:53 +0700

    feat: rename MapMahasiswaDenganRingkasanInterface to MapMahasiswaWithRingkasanInterface for consistency in response data

--- src/types/setoran/service.type.ts
+++ src/types/setoran/service.type.ts
@@ -1,12 +1,12 @@
 import { FindByEmailReturnInterface } from "../dosen/repository.type";
 import { CommonResponse } from "../global.type";
 import { FindRingkasanMahasiswaPAPerAngkatanByDosenNIPReturnInterface } from "../mahasiswa/repository.type";
-import { MapMahasiswaDenganRingkasanInterface } from "./helper.type";
+import { MapMahasiswaWithRingkasanInterface } from "./helper.type";
 
 export interface GetPASayaResponseDataInterface extends FindByEmailReturnInterface {
     info_mahasiswa_pa: {
         ringkasan: FindRingkasanMahasiswaPAPerAngkatanByDosenNIPReturnInterface[],
-        daftar_mahasiswa: MapMahasiswaDenganRingkasanInterface[],
+        daftar_mahasiswa: MapMahasiswaWithRingkasanInterface[],
     };
 }
 
