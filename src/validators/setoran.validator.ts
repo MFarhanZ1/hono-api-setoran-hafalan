8ecb722dc75cc4a3e80eba17cba087433ea92ad1
MFarhanZ1 <farhanaulia.p@gmail.com>
2025-04-20 19:31:39 +0700

    feat: enhance postSetoranSchema validation for tgl_setoran and improve error messages

--- src/validators/setoran.validator.ts
+++ src/validators/setoran.validator.ts
@@ -5,18 +5,27 @@ export const postSetoranSchema = z.object({
 		z.object({
 			nomor_surah: z.number({
 				required_error: "Maaf, tiap nomor_surah wajib diisi yak, mas! 😁",
-				invalid_type_error: "Waduh, tiap nomor_surah wajib berupa angka yak, mas! 😁",
+				invalid_type_error:
+					"Waduh, tiap nomor_surah wajib berupa angka yak, mas! 😁",
 			}),
 			nama_surah: z.string({
 				required_error: "Maaf, tiap nama_surah wajib diisi yak, mas! 😁",
-				invalid_type_error: "Waduh, tiap nama_surah wajib berupa string yak, mas! 😁",
+				invalid_type_error:
+					"Waduh, tiap nama_surah wajib berupa string yak, mas! 😁",
 			}),
 		})
 	),
 	tgl_setoran: z
-		.string()
-		.refine((val) => !val || !isNaN(Date.parse(val)), {
-			message: "Waduh, tgl_setoran wajib berupa format data yang valid yak, mas! 😁",
-		})
+		.union([z.string(), z.null()])
+		.refine(
+			(val) => {
+				if (val === null || val === undefined) return true; // Valid jika null atau undefined
+				return !isNaN(Date.parse(val)); // Validasi hanya jika bukan null
+			},
+			{
+				message:
+					"Waduh, tgl_setoran wajib berupa format tanggal yang valid yak, mas! 😁",
+			}
+		)
 		.optional(),
 });
