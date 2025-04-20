6e050066f4397c56957e5a278d3efe329d23ca0a
MFarhanZ1 <farhanaulia.p@gmail.com>
2025-04-20 17:02:22 +0700

    feat: add findRingkasanPerSyaratByNIM and findDetailByNIM methods to SetoranRepository for detailed report retrieval

--- src/repositories/setoran.repository.ts
+++ src/repositories/setoran.repository.ts
@@ -1,6 +1,6 @@
 import SetoranHelper from "../helpers/setoran.helper";
 import prisma from "../infrastructures/db.infrastructure";
-import { FindAllRingkasanByNIMParamsInterface, FindAllRingkasanByNIMReturnInterface, FindRingkasanByNIMParamsInterface, FindRingkasanByNIMReturnInterface } from "../types/setoran/repository.type";
+import { FindAllRingkasanByNIMParamsInterface, FindAllRingkasanByNIMReturnInterface, FindDetailByNIMParamsInterface, FindDetailByNIMReturnInterface, FindRingkasanByNIMParamsInterface, FindRingkasanByNIMReturnInterface, FindRingkasanPerSyaratByNIMParamsInterface, FindRingkasanPerSyaratByNIMReturnInterface } from "../types/setoran/repository.type";
 
 export default class SetoranRepository {
 	public static async findRingkasanByNIM({ nim }: FindRingkasanByNIMParamsInterface): Promise<FindRingkasanByNIMReturnInterface | null> {
@@ -49,5 +49,70 @@ export default class SetoranRepository {
                 terakhir_setor: SetoranHelper.formatHumanizeDateDiff(mhs.info_setoran.tgl_terakhir_setor),
             }
         }));
-    }    
+    }
+
+    public static async findRingkasanPerSyaratByNIM({ nim }: FindRingkasanPerSyaratByNIMParamsInterface): Promise<FindRingkasanPerSyaratByNIMReturnInterface[] | null> {
+		return await prisma.$queryRaw`
+            SELECT 
+                surah.label "label",
+                COUNT(*)::int "total_wajib_setor",
+                COUNT(setoran.id)::int "total_sudah_setor",
+                (COUNT(*)::numeric - COUNT(setoran.id)::numeric)::int AS "total_belum_setor",
+                ROUND(
+                (COUNT(setoran.id)::numeric / 
+                    COUNT(*)::numeric
+                ) * 100, 2
+                )::float				
+                AS "persentase_progres_setor"
+            FROM
+                surah
+            LEFT JOIN
+                setoran ON setoran.nomor_surah = surah.nomor AND setoran.nim = ${nim}
+            GROUP BY
+                surah.label
+            ORDER BY
+                CASE
+                    WHEN surah.label = 'KP' THEN 1
+                    WHEN surah.label = 'SEMKP' THEN 2
+                    WHEN surah.label = 'DAFTAR_TA' THEN 3
+                    WHEN surah.label = 'SEMPRO' THEN 4
+                    WHEN surah.label = 'SIDANG_TA' THEN 5
+                    ELSE 6
+                END;
+        `;
+	}
+
+    public static async findDetailByNIM({ nim }: FindDetailByNIMParamsInterface): Promise<FindDetailByNIMReturnInterface[] | null> {
+		return await prisma.$queryRaw`
+            SELECT 
+                s.nomor,
+                s.nama,
+                s.label,
+                CASE 
+                    WHEN COUNT(st.id) > 0 THEN true
+                    ELSE false
+                END AS sudah_setor,
+                COALESCE(
+                    JSON_AGG(
+                        JSON_BUILD_OBJECT(
+                            'id', st.id,
+                            'tgl_setoran', st.tgl_setoran,
+                            'tgl_validasi', st.tgl_validasi,
+                            'dosen_yang_mengesahkan', JSON_BUILD_OBJECT(
+                                'nip', st.nip
+                            )
+                        )
+                    ) FILTER (WHERE st.id IS NOT NULL),
+                    null
+                ) -> 0 AS info_setoran 
+            FROM 
+                surah s
+            LEFT JOIN 
+                setoran st ON st.nomor_surah = s.nomor AND st.nim = ${nim}
+            GROUP BY 
+                s.nomor, s.nama, s.label
+            ORDER BY 
+                s.nomor ASC;
+        `
+	}
 }
