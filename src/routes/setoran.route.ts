c08d3f1ba61a4f932b51f749f01ffcd540eda2ae
MFarhanZ1 <farhanaulia.p@gmail.com>
2025-04-22 20:51:08 +0700

    feat: add LogMiddleware to post and delete setoran routes for network information extraction

--- src/routes/setoran.route.ts
+++ src/routes/setoran.route.ts
@@ -5,6 +5,7 @@ import AuthMiddleware from "../middlewares/auth.middleware";
 import { zValidator } from "@hono/zod-validator";
 import { deleteSetoranSchema, postSetoranSchema } from "../validators/setoran.validator";
 import { zodError } from "../utils/zod-error.util";
+import LogMiddleware from "../middlewares/log.middleware";
 
 const setoranRoute = new Hono({ router: new RegExpRouter() });
 
@@ -25,12 +26,14 @@ setoranRoute.get(
 );
 setoranRoute.post(
 	"/mahasiswa/setoran/:nim",
+	LogMiddleware.extractNetworkInformation,
 	zValidator("json", postSetoranSchema, zodError),
 	AuthMiddleware.JWTBearerTokenExtraction,
 	SetoranHandler.postSetoranMahasiswa
 );
 setoranRoute.delete(
 	"/mahasiswa/setoran/:nim",
+	LogMiddleware.extractNetworkInformation,
 	zValidator("json", deleteSetoranSchema, zodError),
 	AuthMiddleware.JWTBearerTokenExtraction,
 	SetoranHandler.deleteSetoranMahasiswa
