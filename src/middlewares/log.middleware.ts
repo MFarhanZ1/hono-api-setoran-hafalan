d3a3d8ac5e1aec8fd5f5996f6fe54c4b6695ac93
MFarhanZ1 <farhanaulia.p@gmail.com>
2025-04-25 20:59:23 +0700

    refactor: improve readability of extractNetworkInformation method in LogMiddleware

--- src/middlewares/log.middleware.ts
+++ src/middlewares/log.middleware.ts
@@ -3,9 +3,13 @@ import { getConnInfo } from "hono/bun";
 
 export default class LogMiddleware {
 	public static async extractNetworkInformation(c: Context, next: Next) {
-        const conn = getConnInfo(c)
+		// retrieve all network information
+		const conn = getConnInfo(c)
 		const ip = c.req.header("x-forwarded-for")?.split(",")[0]?.trim() || conn.remote.address
-		c.set("network_log_data", {ip: ip, user_agent: c.req.header("user-agent")})
+		const user_agent = c.req.header("user-agent")
+		
+		// set network_log_data to context
+		c.set("network_log_data", {ip, user_agent})
 		return next()
 	}
 }
