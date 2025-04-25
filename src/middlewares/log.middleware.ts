9372f6cef906f7d5d1dd9cef845e6013cc913249
MFarhanZ1 <farhanaulia.p@gmail.com>
2025-04-25 20:32:58 +0700

    fix: update network log data extraction to prioritize x-forwarded-for header

--- src/middlewares/log.middleware.ts
+++ src/middlewares/log.middleware.ts
@@ -4,7 +4,8 @@ import { getConnInfo } from "hono/bun";
 export default class LogMiddleware {
 	public static async extractNetworkInformation(c: Context, next: Next) {
         const conn = getConnInfo(c)
-		c.set("network_log_data", {ip: conn.remote.address, user_agent: c.req.header("user-agent")})
+		const ip = c.req.header("x-forwarded-for")?.split(",")[0]?.trim() || conn.remote.address
+		c.set("network_log_data", {ip: ip, user_agent: c.req.header("user-agent")})
 		return next()
 	}
 }
