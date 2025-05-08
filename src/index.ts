4aee66bbdf2806bb7abe3db8a823e4e699470cb8
MFarhanZ1 <farhanaulia.p@gmail.com>
2025-05-08 09:27:13 +0700

    feat: remove CORS middleware from the application

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 import { Hono } from "hono";
-import { cors } from "hono/cors";
 import { RegExpRouter } from "hono/router/reg-exp-router";
 import { BlankEnv, BlankSchema } from "hono/types";
 import GlobalHandler from "./handlers/global.handler";
@@ -13,7 +12,6 @@ const { APP_PORT }: NodeJS.ProcessEnv = process.env;
 
 // Load all available middlewares
 app.use('*', LogMiddleware.hanzLogger);
-app.use('*', cors());
 
 // Load all default routes for common handling
 app.notFound(GlobalHandler.notFound);
