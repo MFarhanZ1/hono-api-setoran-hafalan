bb747ed97dab1c7a712f5a761b2a124b278a1286
MFarhanZ1 <farhanaulia.p@gmail.com>
2025-05-22 22:09:48 +0700

    feat: add PDFGeneratorProps and SurahData interfaces for PDF generation

--- src/types/setoran/helper.type.ts
+++ src/types/setoran/helper.type.ts
@@ -4,3 +4,28 @@ import { InfoSetoranInterface } from "./repository.type";
 export interface MapMahasiswaWithRingkasanInterface extends FindAllMahasiswaPAByDosenNIPReturnInterface {
 	info_setoran: InfoSetoranInterface
 }
+
+export interface PDFGeneratorProps {
+	nama: string;
+	nim: string;
+	dataSurah: SurahData[];
+	dosen_pa: string;
+	nip_dosen: string;
+}
+
+export interface SurahData {
+	id: string;
+	nama: string;
+	label: string;
+	sudah_setor: boolean;
+	info_setoran: Setoran;
+}
+
+interface Setoran {
+	id: string;
+	tgl_setoran: string;
+	tgl_validasi: string;
+	dosen_yang_mengesahkan: {
+		nama: string;
+	};
+}
